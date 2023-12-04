b5c98a0e2c93d29d250edcf9707fd9a2ebbdf26a
RamiOualiEdu <rami.ouali@esprit.tn>
2023-12-04 14:08:30 +0100

    exam

---
+++ src/main/java/tn/esprit/examencorrection/ExamenCorrectionApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.examencorrection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenCorrectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenCorrectionApplication.class, args);
+	}
+
+}
